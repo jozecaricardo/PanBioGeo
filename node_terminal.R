a7cdb05505c647191cbddc9d0cfb12659c7c9bae
Ribeiro, JRI <joseribeiro@unipampa.edu.br>
2024-08-26 11:49:10 -0300

    Updating function with terra package...

--- node_terminal.R
+++ node_terminal.R
@@ -78,10 +78,12 @@ terminal_node <- function(coordin, tree = NULL, shape_file, resol, seeres = FALS
     print('Attention: your shapefile has not been associated to any datum and this routine is going
             to associate it to WGS84 datum!')
     # proj4string(shape_file) <- CRS("+proj=longlat +datum=WGS84") # datum WGS84
-    projection(shape_file) <- CRS("+proj=longlat +datum=WGS84")
+    # projection(as(shape_file, 'Spatial')) <- CRS("+proj=longlat +datum=WGS84")
+    crs(shape_file) <- "+proj=longlat +datum=WGS84"
   } else if(proj4string(as(shape_file, 'Spatial')) != "+proj=longlat +datum=WGS84"){
     # proj4string(shape_file) <- CRS("+proj=longlat +datum=WGS84") # datum WGS84
-    projection(shape_file) <- CRS("+proj=longlat +datum=WGS84")
+    # projection(as(shape_file, 'Spatial')) <- CRS("+proj=longlat +datum=WGS84")
+    crs(shape_file) <- "+proj=longlat +datum=WGS84"
   }
   
   if(seephylog == TRUE){
